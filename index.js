a1cdac1783fd37c33d4be6f6b00d6dd03a8b3a19
khaled al houssein <khaledalhoussein3@gmail.com>
2021-03-30 11:30:31 +0300

    Part 4

--- index.js
+++ index.js
@@ -2,22 +2,35 @@ const { request } = require('express');
 var express = require('express');
 const { reset } = require('nodemon');
 var app = express();
-var port =3000;
+var port = 3000;
 const d = new Date();
-var TIME=d.getHours()+":"+d.getSeconds();
+var TIME = d.getHours() + ":" + d.getSeconds();
 
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}.`);
 });
-app.get("/", function(request,res){
-res.send("ok");
+app.get("/", function (request, res) {
+    res.send("ok");
 });
 
-app.get("/test", function(request,res){
+app.get("/test", function (request, res) {
     res.status(200).send("ok");
-    });
+});
 
-    app.get("/time", function(request,res){
-        let t=TIME;
-        res.status(200).send(t);
-        });
+app.get("/time", function (request, res) {
+    let t = TIME;
+    res.status(200).send(t);
+});
+
+app.get('/hello/:id', function (request, res) {
+    res.status(200).send("Hello"+request.query.id);
+})
+
+app.get('/search?s=:search', function (request, res) {
+    if(request.query.s==""){
+        res.status(500).send("you have to provide a search");
+    }else{
+        res.status(200).send("ok"+request.query.s);
+    }
+    
+})
