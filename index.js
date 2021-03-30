94466b090a15e3afb6fdef9da952d6402f67588d
khaled al houssein <khaledalhoussein3@gmail.com>
2021-03-30 11:17:38 +0300

    Part 3

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express');
 const { reset } = require('nodemon');
 var app = express();
 var port =3000;
+const d = new Date();
+var TIME=d.getHours()+":"+d.getSeconds();
 
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}.`);
@@ -10,3 +12,12 @@ app.listen(port, function () {
 app.get("/", function(request,res){
 res.send("ok");
 });
+
+app.get("/test", function(request,res){
+    res.status(200).send("ok");
+    });
+
+    app.get("/time", function(request,res){
+        let t=TIME;
+        res.status(200).send(t);
+        });
