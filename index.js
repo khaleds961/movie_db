000e071b0568b9c4ef9ded8d3780fae50b882f62
khaled al houssein <khaledalhoussein3@gmail.com>
2021-03-30 11:36:33 +0300

    Part 5

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var app = express();
 var port = 3000;
 const d = new Date();
 var TIME = d.getHours() + ":" + d.getSeconds();
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}.`);
@@ -24,7 +30,7 @@ app.get("/time", function (request, res) {
 
 app.get('/hello/:id', function (request, res) {
     res.status(200).send("Hello"+request.query.id);
-})
+});
 
 app.get('/search?s=:search', function (request, res) {
     if(request.query.s==""){
@@ -33,4 +39,18 @@ app.get('/search?s=:search', function (request, res) {
         res.status(200).send("ok"+request.query.s);
     }
     
-})
+});
+
+app.get('/movies/create', function (request, res) {
+    res.status(200).send("Create");
+});
+app.get('/movies/read', function (request, res) {
+    var m=movies;
+    res.status(200).send(m);
+});
+app.get('/movies/update', function (request, res) {
+    res.status(200).send("Update");
+});
+app.get('/movies/delete', function (request, res) {
+    res.status(200).send("Delete");
+});
